46083423cda37e496106a70f6e2412ea0076e30f
Allah warayo bhatti <allahwarayobhatti6@gmail.com>
2025-02-17 10:12:24 +0500

    Updated

--- src/sanity/schemaTypes/order.ts
+++ src/sanity/schemaTypes/order.ts
@@ -1,66 +1,77 @@
+import { v4 as uuidv4 } from 'uuid';
 
-// schemas/order.js
 export const order = {
-    name: 'order',
-    title: 'Order',
-    type: 'document',
-    fields: [
-      {
-        name: 'customerName',
-        title: 'Customer First Name',
-        type: 'string',
-      },
-      {
-        name: 'customerLastName',
-        title: 'Customer Last Name',
-        type: 'string',
-      },
-      {
-        name: 'email',
-        title: 'Email',
-        type: 'string',
-      },
-      {
-        name: 'address',
-        title: 'Address',
-        type: 'string',
-      },
-      {
-        name: 'city',
-        title: 'City',
-        type: 'string',
-      },
-      {
-        name: 'zip',
-        title: 'Zip Code',
-        type: 'number',
-      },
-      {
-        name: 'phone',
-        title: 'Phone Number',
-        type: 'number',
-      },
-      {
-        name: 'orderItems',
-        title: 'Order Items',
-        type: 'array',
-        of: [
-          {
-            type: 'reference',
-            to: [{ type: 'product' }],
-          },
-        ],
-      },
-      {
-        name: 'totalPrice',
-        title: 'Total Price',
-        type: 'number',
-      },
-      {
-        name: 'createdAt',
-        title: 'Created At',
-        type: 'datetime',
-      },
-    ],
-  };
-  
+  name: 'order',
+  title: 'Order',
+  type: 'document',
+  fields: [
+    {
+      name: 'customerName',
+      title: 'Customer First Name',
+      type: 'string',
+    },
+    {
+      name: 'customerLastName',
+      title: 'Customer Last Name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    },
+    {
+      name: 'city',
+      title: 'City',
+      type: 'string',
+    },
+    {
+      name: 'zip',
+      title: 'Zip Code',
+      type: 'number',
+    },
+    {
+      name: 'phone',
+      title: 'Phone Number',
+      type: 'number',
+    },
+    {
+      name: 'orderItems',
+      title: 'Order Items',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            {
+              name: 'product',
+              title: 'Product',
+              type: 'reference',
+              to: [{ type: 'product' }],
+            },
+            {
+              name: 'quantity',
+              title: 'Quantity',
+              type: 'number',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'totalPrice',
+      title: 'Total Price',
+      type: 'number',
+    },
+    {
+      name: 'createdAt',
+      title: 'Created At',
+      type: 'datetime',
+    },
+  ],
+};
